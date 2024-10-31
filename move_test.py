010be3006f30211c0d41f3f6dd42c5714c38b384
jongcheon <cpsc.jongcheon@gmail.com>
2024-10-31 18:13:43 +0900

    code modification

--- move_test.py
+++ move_test.py
@@ -12,8 +12,8 @@ import numpy as np
 class MoveRobotNode():
     def __init__(self):
         rospy.init_node("MoveRobotNode", anonymous=True)
-        rospy.Subscriber('TargetPCL', PointCloud2, self.callback)
-        rospy.Subscriber('PickingPCL', PointCloud2, self.callback2)
+        rospy.Subscriber('TargetPCL', PointCloud2, self.ConnectorCallback)
+        rospy.Subscriber('PickingPCL', PointCloud2, self.PikcingTargetCallback)
         self.FLAGMSG = rospy.Publisher('ReadyToDetect', Bool, queue_size=1)
         moveit_commander.roscpp_initialize(sys.argv)
         GROUPNAME = "manipulator"
@@ -23,24 +23,29 @@ class MoveRobotNode():
         self.DETECTMSG = Bool()
         self.DETECTMSG.data = True
         self.IMWORKING = True # True로 시작해서 -> GoHome 이동 후 False로 명령 대기
-        self.Set_IO = rospy.ServiceProxy('/ur_hardware_interface/set_io', SetIO)
+        self.SetIO = rospy.ServiceProxy('/ur_hardware_interface/set_io', SetIO)
 
         self.applyVaccum(0)
 
+        # Constant
         self.OFFSET_X = 0.078 # 7.8cm
+        self.TOOL_ORIENTATION = 0.52 # 말단부 Z축 회전 정도(Radian)
+        self.CONNECT_TARGET_HEIGHT = 0.34
+        self.PICKING_TARGET_HEIGHT_1 = 0.18
+        self.PICKING_TARGET_HEIGHT_2 = 0.125
 
-    def callback2(self, input_ros_msg):
+    def ConnectorCallback(self, input_ros_msg):
         gen = pc2.read_points(input_ros_msg, field_names=('x', 'y', 'z', 'rgb'), skip_nans=True)
         int_data = list(gen)[0]
 
-        pointX = int(np.ceil(int_data[0] * 1000)) / 1000
-        pointY = int(np.ceil(int_data[1] * 1000)) / 1000
-        pointZ = int(np.ceil(int_data[2] * 1000)) / 1000
-        print('Robot operating, x : ', pointX, 'y : ', pointY, 'z', pointZ)
+        POINT_X = int(np.ceil(int_data[0] * 1000)) / 1000
+        POINT_Y = int(np.ceil(int_data[1] * 1000)) / 1000
+        POINT_Z = int(np.ceil(int_data[2] * 1000)) / 1000
+        print('Robot operating, x : ', POINT_X, 'y : ', POINT_Y, 'z', POINT_Z)
         if self.IMWORKING == False :
-            self.goConnect(pointX, pointY, pointZ)
+            self.goConnect(POINT_X, POINT_Y, POINT_Z)
 
-    def callback(self, input_ros_msg):
+    def PikcingTargetCallback(self, input_ros_msg):
         gen = pc2.read_points(input_ros_msg, field_names=('x', 'y', 'z', 'rgb'), skip_nans=True)
         int_data = list(gen)[0]
 
@@ -65,58 +70,56 @@ class MoveRobotNode():
         print('Go Home : ', joint_goal)
         move_group.go(joint_goal, wait=True)
         move_group.stop()
-        wpose = move_group.get_current_pose().pose
+        #wpose = move_group.get_current_pose().pose
 
         self.IMWORKING = False
 
-    def goConnect(self, pointX, pointY, pointZ):
+    def goConnect(self, POINT_X, POINT_Y, POINT_Z):
 
         self.IMWORKING = True
         self.move_group.stop()
         self.move_group.clear_pose_targets()
-        pose_goal = geometry_msgs.msg.Pose()
+        TargetPose = geometry_msgs.msg.Pose()
+
 
         # [-1, 0, 0, 0] 하는 이유는 바닥면에 수직한 위치
-        pose_goal.orientation.x = -1
-        pose_goal.orientation.y = 0
-        pose_goal.orientation.z = 0
-        pose_goal.orientation.w = 0
+        # Target에 대한 1차 접
+        TargetPose.orientation.x = -1
+        TargetPose.orientation.y = 0
+        TargetPose.orientation.z = 0
+        TargetPose.orientation.w = 0
 
-        pose_goal.position.x = pointX - self.OFFSET_X - 0.05
-        pose_goal.position.y = pointY
-        pose_goal.position.z = 0.34  # pointZ
+        TargetPose.position.x = POINT_X - self.OFFSET_X - 0.05
+        TargetPose.position.y = POINT_Y
+        TargetPose.position.z = self.CONNECT_TARGET_HEIGHT  # pointZ
 
         # [-1, 0, 0, 0] 에 대한 trajectory를 구한 뒤 마지막 말단부만 joint변경하기 위해 아래 과정 수행
-        self.move_group.set_pose_target(pose_goal)
+        self.move_group.set_pose_target(TargetPose)
         moving_plan = self.move_group.plan()
         target_endpoint = np.asarray(moving_plan[1].joint_trajectory.points[-1].positions)
         # 말단 부 Tip angle 계산 후 수정
-        target_endpoint[-1] = 0.52
-
-        print('다가가는중 .. ', pose_goal)
+        target_endpoint[-1] = self.TOOL_ORIENTATION
         self.move_group.go(target_endpoint, wait=True)
-        print(' 포인트 1 도착 ')
+
 
         # [-1, 0, 0, 0] 하는 이유는 바닥면에 수직한 위치
-        pose_goal.orientation.x = -1
-        pose_goal.orientation.y = 0
-        pose_goal.orientation.z = 0
-        pose_goal.orientation.w = 0
+        # Target에 대한 1차 접근
+        TargetPose.orientation.x = -1
+        TargetPose.orientation.y = 0
+        TargetPose.orientation.z = 0
+        TargetPose.orientation.w = 0
 
-        pose_goal.position.x = pointX - self.OFFSET_X - 0.02
-        pose_goal.position.y = pointY
-        pose_goal.position.z = 0.34  # pointZ
+        TargetPose.position.x = POINT_X - self.OFFSET_X - 0.02
+        TargetPose.position.y = POINT_Y
+        TargetPose.position.z = self.CONNECT_TARGET_HEIGHT  # pointZ
 
         # [-1, 0, 0, 0] 에 대한 trajectory를 구한 뒤 마지막 말단부만 joint변경하기 위해 아래 과정 수행
-        self.move_group.set_pose_target(pose_goal)
+        self.move_group.set_pose_target(TargetPose)
         moving_plan = self.move_group.plan()
         target_endpoint = np.asarray(moving_plan[1].joint_trajectory.points[-1].positions)
         # 말단 부 Tip angle 계산 후 수정
-        target_endpoint[-1] = 0.52
-
-        print('다가가는중 .. ', pose_goal)
+        target_endpoint[-1] = self.TOOL_ORIENTATION
         self.move_group.go(target_endpoint, wait=True)
-        print(' 포인트 1 도착 ')
 
 
     def goPosition(self, pointX, pointY, pointZ):
@@ -134,14 +137,14 @@ class MoveRobotNode():
 
         pose_goal.position.x = pointX - self.OFFSET_X
         pose_goal.position.y = pointY
-        pose_goal.position.z = 0.18#pointZ
+        pose_goal.position.z = self.PICKING_TARGET_HEIGHT_1
 
         # [-1, 0, 0, 0] 에 대한 trajectory를 구한 뒤 마지막 말단부만 joint변경하기 위해 아래 과정 수행
         self.move_group.set_pose_target(pose_goal)
         moving_plan = self.move_group.plan()
         target_endpoint = np.asarray(moving_plan[1].joint_trajectory.points[-1].positions)
         # 말단 부 Tip angle 계산 후 수정
-        target_endpoint[-1] = 0.42
+        target_endpoint[-1] = self.TOOL_ORIENTATION
 
         print('다가가는중 .. ', pose_goal)
         self.move_group.go(target_endpoint, wait=True)
@@ -155,14 +158,14 @@ class MoveRobotNode():
 
         pose_goal.position.x = pointX - self.OFFSET_X
         pose_goal.position.y = pointY
-        pose_goal.position.z = 0.125#pointZ
+        pose_goal.position.z = self.PICKING_TARGET_HEIGHT_2
 
         # [-1, 0, 0, 0] 에 대한 trajectory를 구한 뒤 마지막 말단부만 joint변경하기 위해 아래 과정 수행
         self.move_group.set_pose_target(pose_goal)
         moving_plan = self.move_group.plan()
         target_endpoint = np.asarray(moving_plan[1].joint_trajectory.points[-1].positions)
         # 말단 부 Tip angle 계산 후 수정
-        target_endpoint[-1] = 0.42
+        target_endpoint[-1] = self.TOOL_ORIENTATION
 
         print('다가가는중 .. ', pose_goal)
         self.move_group.go(target_endpoint, wait=True)
@@ -175,7 +178,7 @@ class MoveRobotNode():
 
     def applyVaccum(self, OnOff):
         # OnOff = 1 : On
-        self.Set_IO(fun=1, pin=0, state=OnOff)
+        self.SetIO(fun=1, pin=0, state=OnOff)
 
 if __name__ == "__main__":
     robot_control = MoveRobotNode()
